548f49cfddb72cee19a7a23d707d8e512f7bbdeb
j-bar-8 <josh.barkat@gmail.com>
2018-06-28 19:50:16 -0400

    form is functional

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,54 @@
+// GLOBAL VARIABLES
+// ================================================================================
+var city = ""
+var state = ""
+var startDate ="MM/DD/YYYY"
+var endDate ="MM/DD/YYYY"
+
+
+
+
+
+
+
+
+// FUNCTIONS
+// ==================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+// PROCESS
+// ==================================================================================
 $( document ).ready(function() {
-    console.log( "ready!" );
     
-    $('#startDate').datepicker({
+    console.log( "ready!" );
+// Render Date Picker    
+    $('#startDate-input').datepicker({
         uiLibrary: 'bootstrap4'
     });
-
-    $('#endDate').datepicker({
+    $('#endDate-input').datepicker({
         uiLibrary: 'bootstrap4'
+    });
+// Submit Button
+    $("#submit-form").on("click", function(event) {
+        event.preventDefault();
+        city = $("#city-input").val().trim();
+        state = $("#state-input").val().trim();
+        startDate = $("#startDate-input").val().trim();
+        endDate = $("#endDate-input").val().trim();
+    
+        // TESTING
+        console.log("clicked")
+        console.log(city +"|"+ state +"|"+ startDate +"|"+ endDate)
+        
     });
 });

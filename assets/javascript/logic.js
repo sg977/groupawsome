be3d45c683653f7d09829a5b1ef9a19c99e85eb0
pjmccall <pjmccall13@gmail.com>
2018-06-29 22:49:31 -0400

    seatgeek format mods

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,12 +17,21 @@ var endDate ="MM/DD/YYYY"
 // ==================================================================================
 
 var seatGeek = function() {
+
+    $("#event-view").empty();
+
    console.log(city);
+
+   startDate = $("#startDate-input").val().trim();
+   endDate = $("#endDate-input").val().trim();
+
+   var startDateGeek = startDate.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
+   var endDateGeek = endDate.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");  
     // Event listener for all button elements
         // In this case, the "this" keyword refers to the button that was clicked
   
         // Constructing a URL to search Giphy for the name of the person who said the quote
-        var queryURL = "https://api.seatgeek.com/2/events?datetime_utc.gte=2018-08-01&datetime_utc.lte=2018-08-30&q=" +
+        var queryURL = "https://api.seatgeek.com/2/events?datetime_utc.gte=" + startDateGeek + "&datetime_utc.lte=" + endDateGeek + "&q=" +
           city + "&client_id=Mjc5OTkwOHwxNTMwMDQ1MDAwLjY4";
   
           console.log(queryURL);
@@ -45,7 +54,7 @@ var seatGeek = function() {
               // Only taking action if the photo has an appropriate rating
               if (results[i].stats.average_price > 0 && results[i].performers[0].image !== null) {
                 // Creating a div with the class "item"
-                var eventDiv = $("<div class='item'>");
+                var eventDiv = $("<div class='card' style='width: 18rem;'><div class='card-body'>");
   
                 // Storing the result item's rating
                 var event = results[i].title;
@@ -55,11 +64,11 @@ var seatGeek = function() {
                 var image = results[i].performers[0].image;
   
                 // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Event Title: " + event);
-                var price = $("<p>").text("Average Seat Price: $" + cost + "/each");
-                var type = $("<p>").text("Event Type: " + eventType);
+                var p = $("<h6 class='card-title'>").text("Event Title: " + event);
+                var price = $("<p class='card-text'>").text("Average Seat Price: $" + cost + "/each");
+                var type = $("<p class='card-text'>").text("Event Type: " + eventType);
                 // Creating an image tag
-                var eventImage = '<a href="' + imageURL + '" target="_blank"><img src="' + image + '"></a>'
+                var eventImage = '<a href="' + imageURL + '" target="_blank"><img src="' + image + '" class="card-img-top"></a>'                
   
   
                 // Appending the paragraph and personImage we created to the "gifDiv" div we created
@@ -69,7 +78,7 @@ var seatGeek = function() {
                 eventDiv.append(price);
   
                 // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                $("#event-view").prepend(eventDiv);
+                $("#event-view").append(eventDiv);
                     }
                 }
 
@@ -104,13 +113,17 @@ var seatGeek = function() {
         state = $("#state-input").val().trim();
         startDate = $("#startDate-input").val().trim();
         endDate = $("#endDate-input").val().trim();
+
+        var startDateGeek = startDate.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");
+        var endDateGeek = endDate.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$1-$2");  
+
         // Get Event Info
         seatGeek();
         
     
         // TESTING
         console.log("clicked")
-        console.log(city +"|"+ state +"|"+ startDate +"|"+ endDate)
+        console.log(city +"|"+ state +"|"+ startDateGeek +"|"+ endDateGeek)
     });
     
 });
